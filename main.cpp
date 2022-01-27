a3c6e7bd68a37f679df793f4cbb05b4b1bcd4060
stefan0312 <stefan.muehlehner@bulme.at>
2022-01-27 18:56:09 +0100

    first commit

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
     Webclient w;
     w.show();
 
     return a.exec();
+
 }
--- webclient.h
+++ webclient.h
@@ -1,6 +1,9 @@
 #ifndef WEBCLIENT_H
 #define WEBCLIENT_H
 
+#include <QAbstractSocket>
+#include <QTcpSocket>
+
 #include "ui_webclient.h"
 
 class Webclient : public QWidget, private Ui::Webclient
@@ -8,7 +11,21 @@ class Webclient : public QWidget, private Ui::Webclient
     Q_OBJECT
 
 public:
+
     explicit Webclient(QWidget *parent = nullptr);
+
+private slots:
+
+    void on_Button_clicked();
+
+private:
+
+    QTcpSocket* m_socket;
+    QString m_address;
+    unsigned char m_port = 80;
+
+    void Connected();
+    void ReadyRead();
 };
 
 #endif // WEBCLIENT_H
